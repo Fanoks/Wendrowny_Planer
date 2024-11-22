a5567a883e75b01b34b3ed991b2df7fa94d43406
Jakub Konieczny <126960714+Fanoks@users.noreply.github.com>
2024-11-22 22:15:19 +0100

    Make basic Todo

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from kivy.config import Config
 
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
+from kivy.core.window import Window
 
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
@@ -14,6 +15,10 @@ from kivy.uix.label import Label
 
 Config.set('graphics', 'width', '378')
 Config.set('graphics', 'height', '672')
+
+Window.size = (378, 672)
+Window.borderless = False
+Window.resizable = False
     
 class ItemList(BoxLayout):
     def add_item(self, item: str) -> None:
@@ -21,24 +26,31 @@ class ItemList(BoxLayout):
         self.add_widget(new_label)
 
 class AddItem(BoxLayout):
-    def __init__(self, item_list: ItemList, **kwargs) -> None:
+    item_list: ItemList = None
+
+    def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
-        self.item_list = item_list
+        
+    def add_item_list(self) -> None:
+        self.item_list = self.parent.ids.item_list
 
     def on_add_item_button_click(self) -> None:
-        self.item_list.add_item('Abc')
+        if self.item_list == None:
+            self.add_item_list()
+        self.item_list.add_item(self.ids.todo_name.text)
 
 class MainScreen(FloatLayout):
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
-        self.item_list: ItemList = ItemList()
-        self.add_widget(self.item_list)
-        self.add_widget(AddItem(self.item_list))
+        #self.item_list: ItemList = ItemList()
+        #self.add_item: AddItem = AddItem()
+        #self.add_item.add_item_list(self.item_list)
+        #self.add_widget(self.item_list)
+        #self.add_widget(self.add_item)
 
 class Wendrowny_PlanerApp(App):
     def build(self) -> MainScreen:
-        main_screen: MainScreen = MainScreen()
-        return main_screen
+        return MainScreen()
 
 def main() -> None:
     Wendrowny_PlanerApp().run()
